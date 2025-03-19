d6fb839283cc499008c73260d454c4cc06a92f1c
Laboratorio <laboratorio@uniprojecao.edu.br>
2025-03-19 10:44:23 -0300

    Vetores e Matrizes

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[]args){
+
+        double[] notas = {7.0, 8.5, 9.0, 6.0, 7.5};
+        double soma = 0;
+        for (double nota : notas) {
+            soma += nota;
+
+        }
+        System.out.printf("Média da turma: %.2f", (soma/ notas.length));
+    }
+
+}
