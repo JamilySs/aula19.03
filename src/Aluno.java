4815f6a43a6af1c0c06ca90cfd54627dcd30cda0
Laboratorio <laboratorio@uniprojecao.edu.br>
2025-03-26 08:35:36 -0300

    Exercício Aluno

---
+++ src/Aluno.java
@@ -0,0 +1,18 @@
+public class Aluno {
+    private String nome;
+    private int idade;
+
+    public Aluno(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public String toString() {
+        return "Nome:" + nome + ",idade:" + idade;
+    }
+
+    public static void main(String[] args) {
+        Aluno[]turma = new Aluno[3];
+
+        turma
+    }
+}
